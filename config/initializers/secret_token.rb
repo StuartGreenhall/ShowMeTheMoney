53b7087bd97d64533d62301ccac4b854586c78c8
Stuart Greenhall <stuart_greenhall@hotmail.com>
2011-09-21 20:21:34 +0100

    Stu: Inital commit of skeleton rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoneyMaker::Application.config.secret_token = '825bb69f9450e325d8f62306cdfd2c601c212e40b11020070321e2d117e3603ddf1303c826c65517c30482ee82562c8de4685bf25930a68b860645980fe6a956'
